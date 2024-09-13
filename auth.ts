4bab7e5b16812fd7bc0609b41a9ec9b1bf1631e6
sabovyan <abovyan.sargis90@gmail.com>
2024-09-13 19:07:45 +0400

    initialize auth with prisma

---
+++ auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./services/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+
+  callbacks: {
+    session: ({ session, user }) => {
+      return { ...session, user };
+    },
+  },
+});
